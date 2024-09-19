60148d3d2848fd52096eced86ca0da089d08041c
Josiel Cardoso <josielcardosomercio@gmail.com>
2024-09-19 13:30:04 -0300

    remove login requirement

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./_lib/auth";
+//import { getServerSession } from "next-auth";
+//import { authOptions } from "./_lib/auth";
 import MainPage from "./_components/MainPage";
-import LoginPage from "./_components/LoginPage";
+//import LoginPage from "./_components/LoginPage";
 
 const Page = async () => {
   //const { data: session } = useSession();
-  const session = await getServerSession(authOptions);
-  if (session?.user) {
-    return <MainPage />;
-  } else {
-    return <LoginPage />;
-  }
+  //const session = await getServerSession(authOptions);
+  // if (session?.user) {
+  //   return <MainPage />;
+  // } else {
+  //   return <LoginPage />;
+  // }
+  return <MainPage />;
 };
 
 export default Page;
